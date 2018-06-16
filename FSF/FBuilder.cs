73d59713ecb82d91e7ad77f6909f36c293c712ab
Christos Charalampidis <Chrigeta@hotmail.com>
2018-06-16 20:23:16 +0300

    Added more options

--- FSF/FBuilder.cs
+++ FSF/FBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using FSF.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -15,41 +16,43 @@ namespace FSF
         /// </summary>
         /// <param name="fileRecords">File records object for file</param>
         /// <param name="pathToWrite">Full Path of file to write</param>
-        public static void WriteFile(this FileRecords fileRecords, string pathToWrite)
+        /// <param name="globalFieldBuilder">The Function that will be applied to all field values</param>
+        public static void WriteFile(this FileRecords fileRecords, string pathToWrite, Func<Field, string> globalFieldBuilder = null)
         {
-            IEnumerable<string> fileLines = FileLines(fileRecords);
+            IEnumerable<string> fileLines = FileLines(fileRecords, globalFieldBuilder);
             File.WriteAllLines(pathToWrite, fileLines);
         }
 
         /// <summary>
-        /// Get Ienumerable File Lines
+        /// Get IEnumerable File Lines
         /// </summary>
         /// <param name="fileRecords">File Records of File to build</param>
+        /// <param name="globalFieldBuilder">The Function that will be applied to all field values</param>
         /// <returns>Ienumerable<string> of file lines</returns>
-        public static IEnumerable<string> GetFileLines(this FileRecords fileRecords) => FileLines(fileRecords);
+        public static IEnumerable<string> GetFileLines(this FileRecords fileRecords, Func<Field, string> globalFieldBuilder = null) => FileLines(fileRecords, globalFieldBuilder);
 
-        private static IEnumerable<string> FileLines(FileRecords fileRecords)
+        private static IEnumerable<string> FileLines(FileRecords fileRecords, Func<Field, string> globalFieldBuilder = null)
         {
             var fileLines = new List<string>();
 
             if (fileRecords.Header != null)
-                BuildLineFromFields(fileRecords.Header);
+                BuildLineFromFields(fileRecords.Header, globalFieldBuilder);
 
             foreach (List<Field> record in fileRecords.Records.Values)
             {
-                fileLines.Add(BuildLineFromFields(record));
+                fileLines.Add(BuildLineFromFields(record, globalFieldBuilder));
             }
 
             if (fileRecords.Trailer != null)
-                BuildLineFromFields(fileRecords.Trailer);
+                BuildLineFromFields(fileRecords.Trailer, globalFieldBuilder);
 
             return fileLines;
         }
 
-        private static string BuildLineFromFields(List<Field> fields)
+        private static string BuildLineFromFields(List<Field> fields, Func<Field, string> globalFieldBuilder = null)
         {
             var lineBuilder = new StringBuilder();
-            fields.ForEach(field => lineBuilder.Append(field.Value));
+            fields.ForEach(field => lineBuilder.Append(globalFieldBuilder == null ? field.Value : globalFieldBuilder(field)));
             return lineBuilder.ToString();
         }
     }
